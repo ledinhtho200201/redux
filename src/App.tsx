190c5a228fd36564729b513fb7fe27f1eec3bd70
Pham Tuan <tuan.pv@ptf.com.vn>
2023-11-15 10:24:42 +0700

    update btn saga

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from './components/header';
 import TabsContent from './components/tabs.content';
-import { increment } from './redux/counter/counter.slide';
+import { decrement, increment } from './redux/counter/counter.slide';
 import { useAppDispatch, useAppSelector } from './redux/hooks';
 
 function App() {
@@ -15,8 +15,15 @@ function App() {
       <div>
 
         <div style={{ textAlign: "center" }}>
-          <div>Count = {count}</div>
-          <button className="btn btn-success" onClick={() => dispatch(increment())}>Increase +1</button>
+          <h3>Count = {count}</h3>
+          <div>
+            <button className="btn btn-success mx-3" onClick={() => dispatch(increment())}>Increase +1</button>
+            <button className="btn btn-warning" onClick={() => dispatch(decrement())}>Decrease -1</button>
+          </div>
+          <div className='mt-5'>
+            <button className="btn btn-secondary mx-3" >Increase Saga +1</button>
+            <button className="btn btn-dark" >Decrease Saga -1</button>
+          </div>
         </div>
       </div>
     </>
